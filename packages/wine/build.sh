cfe430609624df0f53aa3cb52a9b5a2a2ec1627d
Syed Furquan Ahmad <syed@nextdev.in>
2025-05-02 17:54:03 +0530

    32-bit Wine build only
    Use latest box64 with box32 enabled

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/KreitinnSoftware/wine
 GIT_COMMIT=36b176851ffce636fc052fab773fb2be8990fe5c
-HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
+HOST_BUILD_CONFIGURE_ARGS="--without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
 OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
